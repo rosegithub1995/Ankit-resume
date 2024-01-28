6cf36bd20015cb75aeb5d71519522b31e806696a
Ankit <ankitprothebro@gmail.com>
2024-01-28 20:31:48 +0530

    uploading about us page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -32,24 +32,33 @@ const TAB_DATA = [
       </div>
     ),
   },
-
   {
     title: "Education",
     id: "education",
     content: (
-      <ul className="list-disc pl-2">
-        <li>Fullstack Academy of Code</li>
-        <li>University of California, Santa Cruz</li>
-      </ul>
+      <div className="grid grid-cols-2 gap-4">
+        <ul className="list-disc pl-2">
+          <li>B. A. English (Hons)</li>
+          <li>Full Stack developer</li>
+          <li>Android Development</li>
+        </ul>
+        <ul className="list-disc pl-2">
+          <li>2021</li>
+          <li>Since May 2022</li>
+          <li>Since 2022</li>
+        </ul>
+      </div>
     ),
   },
   {
-    title: "Certifications",
+    title: "Experiences",
     id: "certifications",
     content: (
       <ul className="list-disc pl-2">
-        <li>AWS Cloud Practitioner</li>
-        <li>Google Professional Cloud Developer</li>
+        <li>Development experience of around 2 years</li>
+        <li className="text-emerald-500 font-semibold">
+          I do not have prior job experience
+        </li>
       </ul>
     ),
   },
@@ -82,73 +91,79 @@ const AboutSection = () => {
   const selectedTab = TAB_DATA.find((t) => t.id === tab) || { content: null };
 
   return (
-    <div className="">
+    <div className="m-2 p-2 gap-4">
       <AnimatePresence mode="wait">
         {isLoading && <LoaderAbout />}
       </AnimatePresence>
 
-      <div className="mt-16 p-6 sm:p-12 dark:bg-gray-900 dark:text-gray-100">
-        <div className="flex flex-col space-y-4 md:space-y-0 md:space-x-6 md:flex-row">
-          <Image
-            src="/images/background.jpg"
-            alt=""
-            height="300"
-            width="300"
-            className="flex-shrink-0 w-96 h-96 border rounded-full md:justify-self-start dark:bg-gray-500 dark:border-gray-700"
-          />
-          <div className="flex flex-col">
-            <h4 className="text-zinc-100 text-2xl md:text-4xl font-semibold text-center md:text-left">
-              Ankit Pratap
-            </h4>
-            <p className="mt-4 text-zinc-300 text-lg dark:text-gray-400">
-              Sure, here's a refined version of your bio with added experience
-              details: "I am a skilled full-stack web developer dedicated to
-              crafting immersive and user-friendly web experiences. With a
-              strong foundation in JavaScript, I specialize in leveraging modern
-              frameworks such as React and Redux to build dynamic front-end
-              interfaces. On the back end, I am proficient in Node.js and
-              Express, utilizing databases like PostgreSQL with Sequelize for
-              efficient data management. My journey in web development has
-              equipped me with a diverse skill set encompassing HTML, CSS, and
-              Git version control. Continuously expanding my knowledge, I stay
-              updated with the latest industry trends and technologies to
-              deliver cutting-edge solutions. As a quick learner, I thrive in
-              fast-paced environments and adapt swiftly to new challenges.
-              Collaboration is at the core of my approach, and I am passionate
-              about working alongside talented teams to bring innovative ideas
-              to life. Together, we create impactful applications that resonate
-              with users and drive business success."
+      <div className="mt-16 p-6 md:p-12 dark:bg-gray-900 dark:text-gray-100">
+        <div className="flex flex-col items-center md:flex-row md:space-x-6">
+          <div className="flex h-96 flex-col space-y-4 md:space-y-0 md:space-x-6 md:flex-row">
+            <Image
+              src="/images/background.jpg"
+              alt=""
+              height="300"
+              width="300"
+              className="flex-shrink-0 w-96 h-96 border 
+            rounded-full md:justify-self-start dark:bg-gray-500 dark:border-gray-700"
+            />
+          </div>
+          <div className="md:w-1/2 max-w-7xl flex flex-col justify-center">
+            <p className="mt-4 text-lg text-zinc-200 dark:text-gray-400">
+              <div className="flex flex-col items-center">
+                <div className="mb-4 text-center">
+                  <h1 className="animate-fade-in-up delay-200 text-3xl font-bold">
+                    Ankit Pratap
+                  </h1>
+                  <h3 className="mt-4 animate-fade-in-up delay-400 text-xl">
+                    Full-Stack Web Developer
+                  </h3>
+                </div>
+                <div className="flex flex-wrap gap-4">
+                  I&apos;m a full-stack web developer with a passion for
+                  crafting user-centric experiences that go beyond pixels and
+                  code. I thrive in collaborative environments and believe in
+                  the power of teamwork to bring innovative ideas to life. My
+                  journey in web development has equipped me with a diverse
+                  skillset, from back-end magic with Node.js and Express to
+                  front-end finesse with React and Redux. But what truly excites
+                  me is seeing my creations resonate with users and contribute
+                  to real-world impact. I&apos;m a quick learner, always eager
+                  to explore new technologies and stay ahead of the curve.
+                  Let&apos;s connect and build something remarkable together!
+                </div>
+              </div>
             </p>
-            <div className="flex pt-8 space-x-4">
-              {TAB_DATA.map((item) => (
-                <TabButton
-                  key={item.id}
-                  selectTab={() => handleTabChange(item.id)}
-                  active={tab === item.id}
-                >
-                  {item.title}
-                </TabButton>
-              ))}
-            </div>
-            <motion.div
-              initial={{ opacity: 0, y: 20 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.5 }}
-              className="mt-2 text-zinc-200"
-            >
-              {selectedTab.content}
-            </motion.div>
           </div>
         </div>
 
-        <div className="text-zinc-200 flex justify-center pt-4 space-x-4 align-center">
-          <RoundedButton>
-            <p>asnemaris@gmail.com</p>
-          </RoundedButton>
-          <RoundedButton>
-            <p>+91 6 27 84 74 30</p>
-          </RoundedButton>
+        <div className="flex mt-4 pt-8 space-x-4 justify-center md:justify-start">
+          {TAB_DATA.map((item) => (
+            <TabButton
+              key={item.id}
+              selectTab={() => handleTabChange(item.id)}
+              active={tab === item.id}
+            >
+              {item.title}
+            </TabButton>
+          ))}
         </div>
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+          className="mt-2 text-zinc-200"
+        >
+          {selectedTab.content}
+        </motion.div>
+      </div>
+      <div className="px-4 space-x-4 md:px-48 text-zinc-200 flex justify-between pt-4 align-center">
+        <RoundedButton>
+          <p>asnemaris@gmail.com</p>
+        </RoundedButton>
+        <RoundedButton>
+          <p>+91 6 27 84 74 30</p>
+        </RoundedButton>
       </div>
     </div>
   );
